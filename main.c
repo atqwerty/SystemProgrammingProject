dc6db074ab872ec6f58ce9f51a6d9ad081cb82c7
Markitanov Denis <dmarkitanov@gmail.com>
2019-03-16 09:51:56 +0600

    added comments

--- main.c
+++ main.c
@@ -1,25 +1,29 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/usb.h>
- 
+
+// output for usb connect interruption
 static int dev_probe(struct usb_interface *interface, const struct usb_device_id *id)
 {
     printk(KERN_INFO "Dev drive (%04X:%04X) plugged\n", id->idVendor, id->idProduct);
     return 0;
 }
- 
+
+// output for usb disconnect interruption
 static void dev_disconnect(struct usb_interface *interface)
 {
     printk(KERN_INFO "Dev drive removed\n");
 }
- 
+
+// hardcoded device table to check which device is plugged (on which device we should interrupt)
 static struct usb_device_id dev_table[] =
 {
     { USB_DEVICE(0x04e8, 0x6860) },
-    {} /* Terminating entry */
+    {} 
 };
 MODULE_DEVICE_TABLE (usb, dev_table);
- 
+
+// device structure
 static struct usb_driver dev_driver =
 {
     .name = "dev_driver",
@@ -27,17 +31,20 @@ static struct usb_driver dev_driver =
     .probe = dev_probe,
     .disconnect = dev_disconnect,
 };
- 
+
+// start of module
 static int __init dev_init(void)
 {
     return usb_register(&dev_driver);
 }
  
+// module destructor
 static void __exit dev_exit(void)
 {
     usb_deregister(&dev_driver);
 }
- 
+
+// modules initialization
 module_init(dev_init);
 module_exit(dev_exit);
 
